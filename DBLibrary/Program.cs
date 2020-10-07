33f28628dfdf9f99547ab389d4040dab2039abbf
va1ery <valery.gourov@gmail.com>
2020-10-07 09:14:25 +0300

    Add project files.

---
+++ DBLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
